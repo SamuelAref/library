d5772c7623f4b5b38753a8f47d21f9415fe768b2
Samuel Arefeaynie <samuelarefeaynie2@gmail.com>
2024-03-07 16:15:50 +0300

    Add library content.

---
+++ index.js
@@ -0,0 +1,4 @@
+import isEven from "./src/functions/isEven";
+import { Person } from "./src/types/person";
+
+export { isEven, Person };
---
+++ src/types/person.d.ts
@@ -0,0 +1,7 @@
+type Age = number;
+
+export interface Person {
+  firstName: string;
+  lastName: string;
+  age: Age;
+}
